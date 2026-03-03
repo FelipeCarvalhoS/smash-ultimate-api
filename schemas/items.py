e099f6225356a2ea75bfe244a32367c3d97c35a2
Felipe de Carvalho Santos <142360153+FelipeCarvalhoS@users.noreply.github.com>
2026-03-03 15:59:08 -0300

    Chore: added item-related schemas

---
+++ schemas/items.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+from enum import StrEnum
+from .shared import SmashGames, Series
+
+
+class ItemType(StrEnum):
+    BATTERING = 'Battering'
+    SPECIAL = 'Special'
+    EXPLOSIVE = 'Explosive'
+    SHOOTING = 'Shooting'
+    THROWING = 'Throwing'
+    TRANSFORMATION = 'Transformation'
+    CONTAINER = 'Container'
+    RECOVERY = 'Recovery'
+    SUMMONING = 'Summoning'
+    STATUS = 'Status'
+
+
+class Item(BaseModel):
+    id: int
+    name: str
+    slug: str
+    series: Series
+    also_appears_in: list[SmashGames]
+    types: list[ItemType]
+    heavy: bool
+    notes: str
+
+    model_config = {'extra': 'forbid'}
+
+
+class ItemQueryParams(BaseModel):
+    ids: list[int] | None = None
+    names: list[str] | None = None
+    series: list[Series] | None = None
+    also_appears_in: list[SmashGames] | None = None
+    types: list[ItemType] | None = None
+    heavy: bool | None = None
