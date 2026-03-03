f36351abc159ca46e00a3e03013dee4a174c85e6
Felipe de Carvalho Santos <142360153+FelipeCarvalhoS@users.noreply.github.com>
2026-03-03 17:27:01 -0300

    Test: added items router tests

---
+++ tests/items/test_router.py
@@ -0,0 +1,34 @@
+from fastapi.testclient import TestClient
+from main import app
+from schemas.items import Item
+
+
+client = TestClient(app)
+
+
+class TestItemRouter:
+    def test_get_item_200(self):
+        response = client.get('/items/80')
+        assert response.status_code == 200
+        data = response.json()
+        assert Item.model_validate(data)
+        assert data['name'] == 'Super Launch Star'
+
+    def test_get_item_404(self):
+        response = client.get('/items/116')
+        assert response.status_code == 404
+
+    def test_get_random_item(self):
+        response = client.get('/items/random')
+        assert response.status_code == 200
+        data = response.json()
+        assert Item.model_validate(data)
+
+    def test_filter_items(self):
+        response = client.get('/items?names=Ball&also_appears_in=64')
+        assert response.status_code == 200
+        data = response.json()
+        assert isinstance(data, list)
+        assert all(Item.model_validate(item) for item in data)
+        items = ['Poké Ball']
+        assert items == [item['name'] for item in data]
