58bd25f765880c6977258c272bc6fa5bb9472ca4
Felipe de Carvalho Santos <142360153+FelipeCarvalhoS@users.noreply.github.com>
2026-03-13 15:20:46 -0300

    Test: added /redoc and /docs tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,8 +6,16 @@ from fastapi import status
 client = TestClient(app)
 
 
-class TestRoot:
+class TestMain:
     def test_get_root(self):
         response = client.get(app.root_path, follow_redirects=False)
         assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
         assert response.headers['location'] == app.root_path + '/redoc'
+
+    def test_get_redoc(self):
+        response = client.get(app.root_path + '/redoc')
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_get_docs(self):
+        response = client.get(app.root_path + '/docs')
+        assert response.status_code == status.HTTP_200_OK
