9cab8797108d8d6d419bd962febe2098bdb1f72d
Felipe de Carvalho Santos <142360153+FelipeCarvalhoS@users.noreply.github.com>
2026-03-13 15:20:35 -0300

    Feat: added favicon

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / 'data'
 R2_URL = os.getenv('R2_URL')
 REPO_URL = os.getenv('REPO_URL')
+FAVICON_URL = R2_URL + 'favicon.png'
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 import config
 from routers import roster_slots, stages, items, fighters
 from fastapi_pagination import add_pagination
 from tags import TAGS_METADATA
 from utils import get_snippet
+from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
 
 
 app = FastAPI(
@@ -15,6 +16,8 @@ app = FastAPI(
     root_path='/beta',
     openapi_tags=TAGS_METADATA,
     openapi_external_docs={'description': 'See project on GitHub', 'url': config.REPO_URL},
+    docs_url=None,
+    redoc_url=None,
 )
 add_pagination(app)
 app.include_router(roster_slots.router)
@@ -23,6 +26,16 @@ app.include_router(items.router)
 app.include_router(fighters.router)
 
 
-@app.get('', include_in_schema=False, response_class=RedirectResponse)
-async def root(request: Request):
+@app.get('', include_in_schema=False)
+async def root(request: Request) -> RedirectResponse:
     return RedirectResponse(url=request.scope['root_path'] + '/redoc')
+
+
+@app.get('/redoc', include_in_schema=False)
+async def redoc() -> HTMLResponse:
+    return get_redoc_html(openapi_url=app.openapi_url, title=app.title, redoc_favicon_url=config.FAVICON_URL)
+
+
+@app.get('/docs', include_in_schema=False)
+async def docs() -> HTMLResponse:
+    return get_swagger_ui_html(openapi_url=app.openapi_url, title=app.title, swagger_favicon_url=config.FAVICON_URL)
